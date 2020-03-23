2349cb03079c43ff54d8843e1df575dc28176ada
Joseph Rivera <josephr98@gmail.com>
2020-03-23 15:44:42 -0400

    completed drill 4

--- scripts/shopping-list.js
+++ scripts/shopping-list.js
@@ -1,5 +1,5 @@
 import store from './store.js';
-import item from './item.js'
+import item from './item.js';
 
 const generateItemElement = function (item) {
   let itemTitle = `<span class="shopping-item shopping-item__checked">${item.name}</span>`;
@@ -43,7 +43,13 @@ const render = function () {
 };
 
 const addItemToShoppingList = function (itemName) {
-  store.items.push({ id: cuid(), name: itemName, checked: false });
+  try {
+    item.validateName(itemName);
+    store.items.push(item.create(itemName));
+    render();
+  } catch(error) {
+    console.log(`Cannot add item: ${error.message}`)
+  }
 };
 
 const handleNewItemSubmit = function () {
